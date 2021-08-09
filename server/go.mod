d5fdd402eb73d9be3f3e7d958166cd522e9b737b
aron-huang-vertiv <aron.huang@vertiv.com>
2021-08-09 14:31:21 +0800

    add server

---
+++ server/go.mod
@@ -0,0 +1,9 @@
+module server
+
+go 1.16
+
+require (
+	github.com/aron-huang-vertiv/grpc_client_create_connection_test v0.0.0-20210809062042-c31e98f7328a
+	google.golang.org/grpc v1.39.1 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+)
---
+++ server/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	foo "github.com/aron-huang-vertiv/grpc_client_create_connection_test/proto/foo"
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":7788"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	foo.RegisterFooServiceServer(s, &FooService{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+
+}
+
+type FooService struct {
+	foo.UnimplementedFooServiceServer
+}
+
+func NewFooService() *FooService {
+	return &FooService{}
+}
+
+func (f FooService) Foo(ctx context.Context, req *foo.FooReq) (*foo.FooResp, error) {
+	return &foo.FooResp{
+		Msg: "Hi "+ req.Name,
+		Count: req.Count,
+	}, nil
+}
